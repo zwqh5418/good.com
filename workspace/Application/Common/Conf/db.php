fbad2db25d812ffbc032c3cec578609f4bd9412e
18829592708 <467099679@qq.com>
2016-07-18 21:46:47 +0800

    修改配置

--- workspace/Application/Common/Conf/db.php
+++ workspace/Application/Common/Conf/db.php
@@ -2,8 +2,8 @@
 return array(
     'DB_TYPE' => 'mysql',
     'DB_HOST' => '127.0.0.1',
-    'DB_USER' => '3315540',
-    'DB_PWD' => 'by8lig',
+    'DB_USER' => 'root',
+    'DB_PWD' => '12345678',
     'DB_PORT' => '3306',
     'DB_NAME' => 'imooc_singcms',
     'DB_CHARSET' => 'utf8',
